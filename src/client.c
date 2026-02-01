467726d3060e4096c8f14b328a61a6e16726d282
ThorsThunder04 <87436831+ThorsThunder04@users.noreply.github.com>
2026-02-01 15:52:53 +0100

    started programming client

---
+++ src/client.c
@@ -0,0 +1,112 @@
+//
+// Created by snaug on 01/02/2026.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+#define SERVER_PORT 16116
+#define SERVER_IP argv[1]
+#define BUFF_SIZE 1024
+
+
+int myRecv(int fd, void* buff, int size, int flags) {
+    
+    int res = recv(fd, (void*)buff, size, flags);
+    if (res == -1) {
+        printf("Error recieving message\n");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+
+    return res;
+}
+
+
+int main(int argc, char **argv)
+{
+    int res = 0;
+    char buff[BUFF_SIZE];
+
+    if (argc != 2) {
+        printf("Expected usage: %s <ip_address>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    // username input
+    char username[25] = {'\0'};
+    do {
+
+        printf("Enter a username (max length 16) : ");
+        scanf("%s", username);
+
+        if (strlen(username) > 16) {
+            printf("Username too long. Try again !\n");
+        } elif (strlen(username) == 0) {
+            printf("You must enter a username. Try again !\n");
+        }
+
+    } while (strlen(username) == 0 || strlen(username) > 16)
+
+
+    // creation of client's socket
+    int sockfd = socket(PF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        printf("Error creating client's socket\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // definition of the server's IP
+    struct sockaddr_in serv_addr;
+    memset(&serv_addr, 0, sizeof(struct sockaddr_in));
+
+    serv_addr.sin_family = PF_INET;
+    serv_addr.sin_port = htons(SERVER_PORT);
+    if (inet_aton(SERVER_IP, &(serv_addr.sin_addr)) == 0) {
+        pritnf("Error parsing IP, verify that it is in IPv4 format\n");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    // connect socket to server
+    res = connect(
+        sockfd,
+        (struct sockaddr*)&serv_addr,
+        sizeof(struct sockaddr)
+    );
+
+    if (res == -1) {
+        printf("Connection error!\n");        
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    // check if server chatroom full
+    res = myRect(sockfd, (void*)buff, sizeof(char), 0);
+    if (res && buff[0] == 'r') {
+        printf("Server Chatroom is full ! Try again later.\n");
+        close(sockfd);
+        exit(1);
+    }
+
+    
+
+    // setup array to be used by seelct()
+    fd_set fds[2] = {stdin, sockfd};
+    int nfds = __max(fds[0], fds[1]);
+
+    fd_set readfds[2];
+    memcpy(readfds, fds, sizeof(fd_set));
+
+    // send username
+
+
+
+
+
+
+    return 0;
+}
